9f471a0d49c076b595aa6e365c19f121f728babc
joan <jenietor0311@gmail.com>
2023-05-27 17:43:08 -0500

    Add method insert, getAll, getById, update y delete

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"gomysql/db"
+	"gomysql/models"
 )
 
 func main() {
 	db.Connect()
 	//fmt.Println(db.CreateTable(models.UserSchema, "users"))
 	//db.Ping()
-	db.TruncateTable("users")
+	//models.CreateUser("Juanita", "123", "juana.nieto@gmail.com")
+	//fmt.Println(user)
+	//db.TruncateTable("users")
+	/*users := models.ListUsers()
+	fmt.Println(users)*/
+	models.Delete(1)
+	users := models.ListUsers()
+	fmt.Println(users)
+	/*user := models.FindById(4)
+	user.Username = "Camila"
+	user.Save()
+	fmt.Println(user)*/
 	db.Close()
 }
--- models/users.go
+++ models/users.go
@@ -1,11 +1,15 @@
 package models
 
+import "gomysql/db"
+
 type User struct {
-	Id       int
+	Id       int64
 	Username string
 	Password string
 	Email    string
 }
+
+type Users []User
 
 const UserSchema string = `Create table users(
 	id int(6) unsigned auto_increment primary key,
@@ -14,3 +18,70 @@ const UserSchema string = `Create table users(
 	email varchar(50),
 	create_data timestamp default current_timestamp
 )`
+
+func newUser(username, password, email string) *User {
+	user := &User{
+		Username: username,
+		Password: password,
+		Email:    email,
+	}
+
+	return user
+}
+
+func CreateUser(username, password, email string) *User {
+	user := newUser(username, password, email)
+	user.insert()
+	return user
+}
+
+func (user *User) insert() {
+	sql := "insert users set username=?, password=?, email=?"
+	result, _ := db.Exec(sql, user.Username, user.Password, user.Email)
+	user.Id, _ = result.LastInsertId()
+}
+
+func ListUsers() Users {
+	sql := "select id, username, password, email from users"
+	users := Users{}
+	rows, _ := db.Query(sql)
+
+	for rows.Next() {
+		user := User{}
+		rows.Scan(&user.Id, &user.Username, &user.Password, &user.Email)
+		users = append(users, user)
+	}
+
+	return users
+}
+
+func FindById(id int) *User {
+	user := newUser("", "", "")
+	sql := "select id, username, password, email from users where id=?"
+	row, _ := db.Query(sql, id)
+
+	for row.Next() {
+		row.Scan(&user.Id, &user.Username, &user.Password, &user.Email)
+	}
+	return user
+}
+
+func (user *User) update() {
+	sql := "update users set username=?, password=?, email=? where id=?"
+
+	db.Exec(sql, user.Username, user.Password, user.Email, user.Id)
+}
+
+func (user *User) Save() {
+	if user.Id == 0 {
+		user.insert()
+	} else {
+		user.update()
+	}
+}
+
+func Delete(id int) {
+
+	sql := "delete from users where id=?"
+	db.Exec(sql, id)
+}
